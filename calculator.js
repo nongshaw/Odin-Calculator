39101df0d0944b1aabf04e762f27016378d7d92f
nongshaw <141841360+nongshaw@users.noreply.github.com>
2023-11-12 13:31:52 +0000

    Correct calculation errors following first calculation. It turned out there were more bugs

--- calculator.js
+++ calculator.js
@@ -3,19 +3,23 @@ let b = 0;              //secondNumber
 let operator = 0;       //operator (add, subtract, multiply, divide)
 
 let displayNumber = 0;  //number shown on display
-// when you press the button, add it to the str.
+// when you press the button
 
 function appendNumber(selection) {
     if (a === 0){ //changes the first number only
         if (displayNumber === 0) {
-            displayNumber = selection; //if display number is 0, change it to the selected number
+            if (selection != 0){ 
+            displayNumber = selection; //if display number is 0, change it to the selected number - but not if first digit is 0
+            }
         } else {
             displayNumber += selection; //otherwise, add the number selected to the end of the string
         }
         document.getElementById("pDisplay").innerHTML = `${displayNumber}`; //update the display with new number
     } else { //changes the second number only
         if (b === 0) {
-            b = selection;
+                if (selection != 0){ 
+                b = selection;
+            }
         } else {
             b += selection;
         }
@@ -23,44 +27,40 @@ function appendNumber(selection) {
     }
 }
 
-function selectOperand(operand) {
-    //changes operator to the one on the button
-    //AND it takes the displayNumber and saves it as a new variable OR just as a... as a...
-    //THEN it adds a " ", the symbol, a " ".
-    operator = operand;
-    a = displayNumber;
-    document.getElementById("pDisplay").innerHTML = `${a}`+" "+`${operand}`+" ";
+function selectOperand(operand) {//changes operator to the one selected, saves displayNumber as a, updates displayNumber to include operand.
+    if (b === 0) {
+        operator = operand;
+        a = displayNumber;
+        document.getElementById("pDisplay").innerHTML = `${a}`+" "+`${operand}`+" ";
+    } else {
+        operate();
+        operator = operand;
+        a = displayNumber;
+        document.getElementById("pDisplay").innerHTML = `${a}`+" "+`${operand}`+" ";
+    }
 }
 
 
 
 
 function operate() {
-    if (operator === "+") {
-        displayNumber = add(a,b);
-    } else if (operator === "-") {
-        displayNumber = subtract(a,b);
-    } else if (operator === "*") {
-        displayNumber = multiply(a,b);
-    } else if (operator === "/") {
-        displayNumber = divide(a,b);
+    if (b != 0) {
+        if (operator === "+") {
+            displayNumber = parseInt(a) + parseInt(b);
+        } else if (operator === "-") {
+            displayNumber = a - b;
+        } else if (operator === "*") {
+            displayNumber = a * b;
+        } else if (operator === "/") {
+            displayNumber = a / b;
+        }
+        document.getElementById("pDisplay").innerHTML = `${displayNumber}`;
+        b = 0;
     }
-    document.getElementById("pDisplay").innerHTML = `${displayNumber}`;
+}
+function clearDisplay() {
+    a = 0;
     b = 0;
-}
-
-function add(a,b) {
-    return a + b;
-}
-
-function subtract(a,b) {
-    return a - b;
-}
-
-function multiply(a,b) {
-    return a * b;
-}
-
-function divide(a,b) {
-    return a / b;
+    displayNumber = 0;
+    document.getElementById("pDisplay").innerHTML = `${displayNumber}`;
 }
